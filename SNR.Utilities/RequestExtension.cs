25ae10d4bfa4a6dedd39ae3dcf8e02354b2fe946
soner.acar <soner.acar@vbt.com.tr>
2022-05-03 01:13:14 +0300

    SNR pre-name added.

--- SNR.Utilities/RequestExtension.cs
+++ SNR.Utilities/RequestExtension.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Net;
 
-namespace Utilities;
+namespace SNR.Utilities;
 
 public static class RequestExtension
 {
--- SNR.Utilities/StringHelper.cs
+++ SNR.Utilities/StringHelper.cs
@@ -1,4 +1,4 @@
-﻿namespace Utilities;
+﻿namespace SNR.Utilities;
 
 public static class StringHelper
 {
